d272b55a5d65bce328c1bcc3add61652090e9e4b
duarteicaros <icaroduartec@gmail.com>
2021-04-12 21:52:16 -0300

    Implementação da Classificação

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ const observacoesPorLembreteId = {};
 const {
     v4: uuidv4
 } = require('uuid');
+
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes =
+            observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+
+        obsParaAtualizar.status = observacao.status;
+        axios.post('http://localhost:10000/eventos', {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        });
+    }
+}
+
+app.post("/eventos", (req, res) => {
+    try{
+        funcoes[req.body.tipo](req.body.dados);
+    }
+    catch(err){}
+    res.status(200).send({
+        msg: "ok"
+    });
+});
+
+
 //:id é um placeholder
 //exemplo: /lembretes/123456/observacoes
 
@@ -25,17 +56,19 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         observacoesPorLembreteId[req.params.id] || [];
     observacoesDoLembrete.push({
         id: idObs,
-        texto
+        texto,
+        status: 'aguardando'
     });
     observacoesPorLembreteId[req.params.id] =
         observacoesDoLembrete;
-        //acrescentar essa linha depois da aula barreamento de eventos
+    //acrescentar essa linha depois da aula barreamento de eventos
     await axios.post('http://localhost:10000/eventos', {
         tipo: "ObservacaoCriada",
         dados: {
             id: idObs,
             texto,
-            lembreteId: req.params.id
+            lembreteId: req.params.id,
+            status: 'aguardando'
         }
     })
     //daqui pra baixo, normal
